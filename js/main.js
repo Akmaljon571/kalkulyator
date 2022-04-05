cc3a09a8b586e7750403f52498173a4f023e12c6
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-04-05 16:27:00 +0300

    kalkulyator

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let son1 = Number(prompt('1-raqam kiriting'));
+let amal = prompt('amalni kiriting kiriting');
+let son2 = Number(prompt('2-raqam kiriting'));
+
+if (amal == 0){
+  alert('amal kiritmadingiz')  
+}
+
+if(son1 && son2){
+    
+    if (amal == '+'){
+        console.log(son1+son2)
+
+    } else if (amal == '-'){
+        console.log(son1 - son2);
+    }else if (amal == '*'){
+        console.log(son1 * son2);
+    } else if (amal == '/'){
+        console.log(son1 / son2);
+    } 
+     else {
+      alert('Hato amal kiritdingiz')
+      console.error('xato amal kiritingiz')
+    }
+
+}
+
+if (son1){
+   
+} else {
+    alert('1-kiritgan soningiz hato')
+}
+if (son2){
+   
+} else {
+    alert('2-kiritgan soningiz hato')
+}
+
